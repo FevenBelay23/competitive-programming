a0001cc8d0f764742c7898b8b3f8a1e82dea7b03
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2022-09-16 15:50:49 +0300

    Time: 357 ms (26.29%), Space: 14.5 MB (82.62%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        i=0
+        j=0
+        while i<len(nums):
+            if nums[i]==0:
+                nums.pop(i)
+                j+=1
+                i-=1
+            i+=1
+        for k in range(j):
+            nums.append(0)
