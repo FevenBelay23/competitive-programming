6684a54ea3811d4a936076447100102bbe26061e
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2023-01-29 00:29:13 +0300

    Update 1859-sorting-the-sentence.py

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -4,10 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        w = s.split(" ")
-        t = [0]*len(w)
-        for i in range(0,len(w)):
-            t[int(w[i][-1])-1] = w[i][0:-1]
-        return ' '.join(t)
+        splited= s[::-1].split() 
+        splited.sort()     
+        answer = []  
+        for word in splited:  
+            answer.append(word[1:][::-1]) 
+        return " ".join(answer) 
 
 
