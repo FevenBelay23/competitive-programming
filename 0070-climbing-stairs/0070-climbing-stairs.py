41008aacaed7d47be8c9ba774d412f73f37ff6ab
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2023-05-31 10:43:04 +0300

    Time: 49 ms (19.51%), Space: 16.3 MB (35.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        climb = {}
-        def stairs(n):
-            if n <= 2:
-                return n
-            if n not in climb:
-                climb[n] = stairs(n - 1) + stairs(n - 2)
-            return climb[n]
-        return stairs(n)
+        first , second = 1,1
+        for i in range(n-1):
+            temp = first
+            first = first + second
+            second = temp
+        return first 
