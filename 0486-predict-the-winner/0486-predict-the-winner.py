d6e2ea01c534ef3d6e6e070fc27c7a54f502abc4
FevenBelay23 <91492132+FevenBelay23@users.noreply.github.com>
2023-08-02 23:28:59 +0300

    Time: 2931 ms (14.20%), Space: 16.2 MB (83.77%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,11 +1,10 @@
 class Solution:
-    def PredictTheWinner(self, nums: List[int]) -> bool:
-        n = len(nums)
-        dp = [[0] * n for _ in range(n)]
-        for i in range(n):
-            dp[i][i] = nums[i]
-        for k in range(1, n):
-            for i in range(n - k):
-                j = i + k
-                dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1])
-        return dp[0][n - 1] >= 0
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def winner(start, end):
+            if start == end:
+                return nums[start]
+            first = nums[start] - winner(start + 1, end)
+            last = nums[end] - winner(start, end - 1)
+            return max(first, last)
+
+        return winner(0, len(nums) - 1) >= 0
